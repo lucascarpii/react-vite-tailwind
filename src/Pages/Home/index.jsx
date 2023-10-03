f49e4f671ce3580e3636a7eff83912f955fdb386
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-10-02 21:10:57 -0300

    <feat> Consumimos la API y ordenamos las cards en un grid

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from "react"
 import { Card } from "../../Components/Card"
 import { Layout } from "../../Components/Layout"
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(()=>{
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
+
   return (
     <Layout>
-      <h1>Home</h1>
-      <Card />
+      <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
+      {
+        items?.map(item => (
+          <Card key={item.id} data={item} />
+        ))
+      }
+      </div>
     </Layout>
   )
 }
