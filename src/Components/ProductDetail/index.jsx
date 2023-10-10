7781557b825ebce024e2754964a0525f430939d3
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-10-10 11:54:53 -0300

    <style> Estructura, colores y svg en ProductDetail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,7 +1,14 @@
 const ProductDetail = () => {
   return (
-    <aside className="w-[360px]">
-
+    <aside className="w-[360px] border flex flex-col fixed rounded-lg bg-white right-0 h-[calc(100vh_-_68px)] top-[68px]">
+      <div className="flex justify-between items-center p-6">
+        <h2 className="font-medium text-xl text-gray-600">Detail</h2>
+        <button className="p-1 text-gray-500 rounded-lg border hover:bg-gray-100">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
     </aside>
   )
 }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { Card } from "../../Components/Card"
 import { Layout } from "../../Components/Layout"
+import { ProductDetail } from "../../Components/ProductDetail"
 
 function Home() {
   const [items, setItems] = useState(null)
@@ -22,6 +23,7 @@ function Home() {
         ))
       }
       </div>
+      <ProductDetail />
     </Layout>
   )
 }
