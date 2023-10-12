a72de39188bc030bb1bbec0aed04426eea370b29
Lucas Fuentes <lucascarpii2002@gmail.com>
2023-10-12 10:47:30 -0300

    <feat> OrderCart Copmonent

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,9 +1,10 @@
 import { useContext } from "react"
 import { ShoppingCartContext } from "../../Context"
+import { OrderCard } from "../OrderCard"
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
-
+  console.log(context.cartProducts)
   return (
     <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} w-[360px] shadow-lg border flex flex-col fixed rounded-lg bg-white right-0 h-[calc(100vh_-_68px)] top-[68px]`}>
       <div className="flex justify-between items-center px-4 py-4 border-b">
@@ -13,6 +14,17 @@ const CheckoutSideMenu = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
+      </div>
+      <div className="px-4 py-4">
+      {
+        context.cartProducts.map(product => (  
+        <OrderCard 
+          key={product.id}
+          title={product.title}
+          imageUrl={product.images?.[0]}
+          price={product.price} />
+          ))
+      }
       </div>
     </aside>
   )
